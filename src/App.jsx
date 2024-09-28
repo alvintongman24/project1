9dcf32b59f4f48f31347d727a6785e38bc467d4a
alvintongman24 <alvintongman24@gmail.com>
2024-09-28 17:35:48 +0100

    removed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import Banner from "./components/Banner/Banner";
 import Subscribe from "./components/Subscribe/Subscribe";
 import Banner2 from "./components/Banner/Banner2";
 import Footer from "./components/Footer/Footer";
-import Navbar from "./Components/Navbar/Navbar";
+// import Navbar from "./Components/Navbar/Navbar";
 import Video from "./components/Video/Video";
 
 const App = () => {
   return (
     <main className="overflow-x-hidden bg-white text-dark">
-      <Navbar />
+      {/* <Navbar /> */}
       <Hero />
       <Works />
       <Banner />
