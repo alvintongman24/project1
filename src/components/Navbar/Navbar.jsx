211a8b31595ce926a6ddc45ecf47040b15ab01ce
alvintongman24 <alvintongman24@gmail.com>
2024-09-28 17:28:22 +0100

    fixed nav bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
           onClick={() => setToggle(!toggle)}
         />
 
-        <div
+        {/* <div
           className={`${
             !toggle ? "hidden" : "flex"
           } p-6 bg-gray-400 absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
@@ -74,7 +74,7 @@ const Navbar = () => {
               </li>
             ))}
           </ul>
-        </div>
+        </div> */}
       </div>
     </nav>
   );
